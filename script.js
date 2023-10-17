2fe99c55c3d08758910cbd2687933dcbfbc8490c
2.E Šindelář Antonín <Antonin.Sindelar@student.sps-cl.cz>
2023-10-17 10:20:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function fibCislo(n){
+    if (n < 2) return 1 
+        return fibCislo(n -1) + fibCislo (n - 2);
+}
+
+function vypocitej (){
+    alert(fibCislo(Number(document.getElementById("vstup").value)))
+}
